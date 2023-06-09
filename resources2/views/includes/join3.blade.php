1e8880a4feaac2f831a2d7cff64f8598f9ee647f
AhmedAljalabi <134627965+AhmedAljalabi@users.noreply.github.com>
2023-06-09 03:04:01 +0300

    Add files via upload

---
+++ resources2/views/includes/join3.blade.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>   التطوع في المنظمة   </title>
+	  <style type="text/css">
+    .aaasss{display: flex;justify-content:center; align-items: center;  }
+    .i{display: grid; place-items:center;}
+    .j{display: flex;justify-content:center;}
+    .ttt{  direction: rtl; unicode-bidi: bidi-override;   text-align: center; }
+    .leftside{ direction: rtl; position:relative; right: 10%;}
+    .centerside{direction: rtl; position:center;}
+button {
+  --border: 5px;    /* the border width */
+  --slant: 0.7em;   /* control the slanted corners */
+  --color: #37E8FC; /* the color */
+  
+  font-size: 35px;
+  padding: 0.4em 1.2em;
+  border: none;
+  cursor: pointer;
+  font-weight: bold;
+  color: var(--color);
+  background: 
+     linear-gradient(to bottom left,var(--color)  50%,#0000 50.1%) top right,
+     linear-gradient(to top   right,var(--color)  50%,#0000 50.1%) bottom left;
+  background-size: calc(var(--slant) + 1.3*var(--border)) calc(var(--slant) + 1.3*var(--border));
+  background-repeat: no-repeat;
+  box-shadow:
+    0 0 0 200px inset var(--s,#0000),
+    0 0 0 var(--border) inset var(--color);
+  clip-path: 
+      polygon(0 0, calc(100% - var(--slant)) 0, 100% var(--slant),
+              100% 100%, var(--slant) 100%,0 calc(100% - var(--slant))
+             );
+  transition: color var(--t,0.3s), background-size 0.3s;
+}
+button:focus-visible {
+  outline-offset: calc(-1*var(--border));
+  outline: var(--border) solid #000c;
+  clip-path: none;
+  background-size: 0 0;
+}
+button:hover,
+button:active{
+  background-size: 100% 100%;
+  color: #fff;
+  --t: 0.2s 0.1s;
+}
+button:active {
+  --s: #0005;
+  transition: none;
+}
+
+ </style>
+
+</head>
+<body>
+
+<div class="j"> 
+
+<font class="headline"> &nbsp; &nbsp;        تقديم وظيفة في المنظمة     &nbsp;&nbsp;  </font>
+
+
+
+</div>
+
+<p>&nbsp;</p>
+
+ 
+
+
+<div class="j"> 
+
+
+
+
+
+
+</div>
+
+<p>&nbsp;</p>
+<div class="leftside">
+
+ <font size="6"    color="black" >
+<p>
+  <u>
+<b>
+
+الرجاء ارسال كافة البيانات مع اثبات الهوية الي البريد أسفل الصفحة :-
+</b>
+</u>
+</p>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+</font>
+
+ <font size="6"    color="gray"  >
+  <ul>
+    <li>
+<p>
+1- الاسم رباعي.
+</p>
+</li>
+
+<li>
+<p>
+2- التخصص الجامعي.
+</p>
+</li>
+
+<li>
+<p>
+3- العمل الحالي.
+</p>
+</li>
+
+<li>
+<p>
+4- رقم الهاتف.
+</p>
+</li>
+
+<li>
+<p>
+5- رقم هاتف العمل ان وجد.
+</p>
+</li>
+
+<li>
+<p>
+6- السكن.
+</p>
+</li>
+
+<li>
+</p>
+7- البريد الالكتروني الشخصي.
+</p>
+</li>
+
+</ul>
+</font>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+ <font size="6"    color="black"  >
+  <b>
+</p>
+<u>
+بريد المنظمة الالكتروني:-
+</u>
+</p>
+</b>
+
+</font>
+
+
+<marquee behavior="slide" direction="up"  scrollamount="1">
+<p>&nbsp;</p>
+ <font size="8"   color="#800ccc" >
+</p>
+SVO-net.org@gmail.com
+</p>
+</font>
+
+</marquee>
+</div>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+<div class="aaasss " >
+
+ <font size="5"    color="#800ccc" style="position: center;">
+
+<p>
+<marquee behavior="slide" direction="down"  scrollamount="1">
+<p>&nbsp;</p>
+<b>
+شكرا لتطوعك و مرحبا بك في اسرة المنظمة
+</b>
+
+</marquee>
+
+</p>
+
+</font>
+
+</div>
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<div class="j">
+<a href="#"> <button style="--color:#00f0ff;--border:2px;--slant:.5em">
+   أتصل بنا      </button></a>&nbsp;&nbsp;
+
+
+<a href="#"> <button style="--color:#d6ff00;--border:2px;--slant:.5em">
+   العروض      </button></a>&nbsp;&nbsp;
+
+   <a href="#"> <button style="--color:#f3738a;--border:2px;--slant:.5em">
+   الخدمات      </button></a>&nbsp;&nbsp;
+
+<a href="#"> <button style="--color:#69db3a;--slant:.5em;--border:2px">  عن المنظمة    </button></a>&nbsp;&nbsp;
+
+<a href="#"> <button style="--color:#800ccc;--slant:.5em;--border:2px">     الرئيسية     </button></a>&nbsp;&nbsp;  
+</div>
+<p>&nbsp;</p>
+
+</body>
+</html>
